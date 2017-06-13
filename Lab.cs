89d13c9cec6ca99a3d5e72d86bad368c8629a697
Anthony Gillespie <anthonygillespie60@gmail.com>
2017-06-13 10:15:37 -0400

    Create new project

---
+++ Lab.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { while(true)
+            { Console.WriteLine("How far did you run");
+                double milesRun = Double.Parse(Console.ReadLine());
+                Console.WriteLine("How many minutes did you run?");
+                double minutesRan = Double.Parase(Console.ReadLine());
+
+                double avgMileTime = Math.Round(minutesRan / milesRan, 2);
+
+                Console. WriteLine("Average pace: " +avgMileTime + "minutes per mile")
+                    if (previousPace ==)
+            }
+        }
+    }
+}
